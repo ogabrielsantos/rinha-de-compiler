a338c92a840b725fe06de6e6aa0b7fdfe216286b
Gabriel Santos <gabriel@ogabrielsantos.com.br>
2023-09-20 14:54:40 -0300

    feat: move to_ast_node to utils and add tests

---
+++ tests/utils/test_to_ast_node.py
@@ -0,0 +1,51 @@
+import pytest
+from precisely import assert_that, all_elements, is_instance
+
+from utils import to_ast_node
+from nodes import BinaryOp, Var, Function, Call, Let, Str, Int, Bool, If, Binary, Tuple, First, Second, Print, Parameter
+
+
+class TestToAstNode:
+    def test_should_not_modify_if_not_a_node(self):
+        assert to_ast_node({}) == {}
+
+    def test_should_not_modify_if_is_an_unsupported_node(self):
+        assert to_ast_node({"kind": "Unsupported"}) == {"kind": "Unsupported"}
+
+    @pytest.mark.parametrize(
+        "data, expected_instance",
+        [
+            [{"kind": Var.kind, "text": "foo"}, Var],
+            [{"kind": Function.kind, "parameters": [], "value": "foo"}, Function],
+            [{"kind": Call.kind, "callee": "foo", "arguments": []}, Call],
+            [{"kind": Let.kind, "name": {"text": "foo"}, "value": "bar", "next": None}, Let],
+            [{"kind": Str.kind, "value": "foo"}, Str],
+            [{"kind": Int.kind, "value": 1}, Int],
+            [{"kind": Bool.kind, "value": True}, Bool],
+            [{"kind": If.kind, "condition": "foo", "then": "foo", "otherwise": "foo"}, If],
+            [{"kind": Binary.kind, "lhs": "foo", "op": BinaryOp.Eq, "rhs": "foo"}, Binary],
+            [{"kind": Tuple.kind, "first": 10, "second": 20}, Tuple],
+            [{"kind": First.kind, "value": (10, 20)}, First],
+            [{"kind": Second.kind, "value": (10, 20)}, Second],
+            [{"kind": Print.kind, "value": "foo"}, Print],
+        ]
+    )
+    def test_should_return_kind_instance_if_is_a_supported_node(self, data, expected_instance):
+        result = to_ast_node(data)
+
+        assert isinstance(result, expected_instance)
+
+    def test_should_convert_op_to_binary_op(self):
+        result = to_ast_node({"kind": Binary.kind, "lhs": "foo", "op": "Eq", "rhs": "foo"})
+
+        assert isinstance(result.op, BinaryOp)
+
+    def test_should_convert_name_to_parameter(self):
+        result = to_ast_node({"kind": Let.kind, "name": {"text": "foo"}, "value": "bar", "next": None})
+
+        assert isinstance(result.name, Parameter)
+
+    def test_should_convert_parameters_to_parameter_instances(self):
+        result = to_ast_node({"kind": Function.kind, "parameters": [{"text": "foo"}, {"text": "bar"}], "value": "foo"})
+
+        assert_that(result.parameters, all_elements(is_instance(Parameter)))
--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from nodes import Parameter, BinaryOp
+from nodes import Parameter, BinaryOp, Var, Function, Call, Let, Str, Int, Bool, If, Binary, Tuple, First, Second, Print
 
 
 def identity(x):
@@ -24,3 +24,31 @@ def with_binary_operation(object_data: dict):
     mapped_op = BinaryOp(op)
 
     return {**object_data, "op": mapped_op}
+
+
+SUPPORTED_NODES = {
+    Var.kind: (Var, identity),
+    Function.kind: (Function, with_parameters),
+    Call.kind: (Call, identity),
+    Let.kind: (Let, with_parameter),
+    Str.kind: (Str, identity),
+    Int.kind: (Int, identity),
+    Bool.kind: (Bool, identity),
+    If.kind: (If, identity),
+    Binary.kind: (Binary, with_binary_operation),
+    Tuple.kind: (Tuple, identity),
+    First.kind: (First, identity),
+    Second.kind: (Second, identity),
+    Print.kind: (Print, identity),
+}
+
+
+def to_ast_node(object_data: dict):
+    if kind := object_data.get("kind"):
+        if node_handler := SUPPORTED_NODES.get(kind):
+            node, handler = node_handler
+            node_arguments = handler(object_data)
+
+            return node(**node_arguments)
+
+    return object_data
