3467c409bdc9a80e6d137b01fd43d4a7aec57dd8
Gabriel Santos <gabriel@ogabrielsantos.com.br>
2023-09-20 14:25:47 -0300

    chore: format code

--- utils.py
+++ utils.py
@@ -9,11 +9,11 @@ def with_parameters(object_data: dict):
     parameters = object_data.get("parameters", [])
     mapped_parameters = list(map(lambda parameter: Parameter(**parameter), parameters))
 
-    return {**object_data, 'parameters': mapped_parameters}
+    return {**object_data, "parameters": mapped_parameters}
 
 
 def with_parameter(object_data: dict):
     parameter = object_data["name"]
     mapped_parameter = Parameter(**parameter)
 
-    return {**object_data, 'name': mapped_parameter}
+    return {**object_data, "name": mapped_parameter}
